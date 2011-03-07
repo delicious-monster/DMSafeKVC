29d3f7f9eb54210edaa7d731377497c2fd6fda6d
Jonathon Mah <jonathon@delicious-monster.com>
2011-03-06 17:12:02 -0800

    When defining type existence, put key name first
    This way we can let the key argument start with *, which further lets us
    define multiple keys at the same time.

--- DMSafeKVC.h
+++ DMSafeKVC.h
@@ -10,13 +10,21 @@
 
 
 // Declares a safe-KVC-accessible property
-// Use as: @property (opts) MyType *_KVC(name);
-#define _KVC(NAME) \
-    NAME; SAFE_KVC(NAME)
+// Use as: @property (opts) MyType KVC(*name);
+// Use as: @property (opts) MyType KVC2(*firstName, *lastName);
+#define KVC(a) \
+    a; SAFE_KVC(a)
+#define KVC2(a, b) \
+    a, b; SAFE_KVC(a); SAFE_KVC(b)
+#define KVC3(a, b, c) \
+    a, b, c; SAFE_KVC(a); SAFE_KVC(b); SAFE_KVC(c)
+#define KVC4(a, b, c, d) \
+    a, b, c, d; SAFE_KVC(a); SAFE_KVC(b); SAFE_KVC(c); SAFE_KVC(d)
 
 // Declares a specific safe-KVC-accessible key outside of a property
+// Note that this will be expanded as returning "void *" for pointer properties
 #define SAFE_KVC(NAME) \
-    void _DMSafeKVC__ ## NAME (void)
+    void NAME ## __KVCPath_(void)
 
 
 // Common safe KVC defines
@@ -36,7 +44,7 @@ SAFE_KVC(selectionIndexes);
 #define KeyPath(...) \
     DMMakeKeyPath(__VA_ARGS__, nil)
 #define K(x) \
-    __builtin_choose_expr(1, @#x, (NSString *)sizeof(_DMSafeKVC__ ## x))
+    __builtin_choose_expr(1, @#x, (NSString *)sizeof(x ## __KVCPath_))
 
 
 extern NSString *DMMakeKeyPath(NSString *firstKey, ...);
